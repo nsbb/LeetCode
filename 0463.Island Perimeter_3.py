8f438e20234886a8c2c456c04b93c9fa1e1f5fe6
nsbb <memorychib@gmail.com>
2021-10-05 00:26:29 +0900

    Update 0463.Island Perimeter_3.py

--- 0463.Island Perimeter_3.py
+++ 0463.Island Perimeter_3.py
@@ -4,7 +4,7 @@ class Solution:
         len_i=len(grid); len_j=len(grid[0])
         for i in range(len_i):
             for j in range(len_j):
-                if i==0 and j==0:                   #top left edge
+                if i==0 and j==0:                                    #top left edge
                     if grid[i][j] == 1:
                         result+=2
                     if grid[i][j] != grid[i][j+1]:
@@ -18,37 +18,37 @@ class Solution:
                         result+=1
                     if grid[i][j] != grid[i+1][j]:
                         result+=1
-                elif i==0 and j==(len_j-1):         #top rirght edge
+                elif i==0 and j==(len_j-1):                          #top rirght edge
                     if grid[i][j] == 1:
                         result+=2
                     if grid[i][j] != grid[i+1][j]:
                         result+=1
-                elif j==0 and i!=0 and i!=(len_i-1):                         #left edge
+                elif j==0 and i!=0 and i!=(len_i-1):                 #left edge
                     if grid[i][j] == 1:
                         result+=1
                     if grid[i][j] != grid[i][j+1]:
                         result+=1
                     if grid[i][j] != grid[i+1][j]:
                         result+=1
-                elif i==(len_i-1) and j==0:         #bottom left edge
+                elif i==(len_i-1) and j==0:                          #bottom left edge
                     if grid[i][j] == 1:
                         result+=2
                     if grid[i][j] != grid[i][j+1]:
                         result+=1
-                elif i==(len_i-1) and j!=0 and j!=(len_j-1):                 #bottome edge                
+                elif i==(len_i-1) and j!=0 and j!=(len_j-1):         #bottome edge                
                     if grid[i][j] == 1:
                         result+=1
                     if grid[i][j] != grid[i][j+1]:
                         result+=1
-                elif i==(len_i-1) and j==(len_j-1): #bottom right edge
+                elif i==(len_i-1) and j==(len_j-1):                  #bottom right edge
                     if grid[i][j] == 1:
                         reseult += 2
-                elif j==(len_j-1) and i!=0 and i!=(len_i-1):                 #right edge
+                elif j==(len_j-1) and i!=0 and i!=(len_i-1):         #right edge
                     if grid[i][j] == 1:
                         result+=1
                     if grid[i][j] != grid[i+1][j]:
                         result+=1                    
-                else:
+                else:                                                #middle
                     if grid[i][j] != grid[i][j+1]:
                         result+=1
                     if grid[i][j] != grid[i+1][j]:

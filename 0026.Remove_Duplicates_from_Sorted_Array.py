0ae302b683182a5783647262e1195a70975bbeeb
nsbb <memorychib@gmail.com>
2021-10-10 22:57:36 +0900

    Update 0026.Remove_Duplicates_from_Sorted_Array.py

--- 0026.Remove_Duplicates_from_Sorted_Array.py
+++ 0026.Remove_Duplicates_from_Sorted_Array.py
@@ -1,5 +1,7 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         count=0
+        va=''
         for i,x in enumerate(nums):
-            
+            va=va+str(x)
+        if 
